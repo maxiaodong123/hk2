8dd851d1f3b54c15151213372c8b0c703f742cab
马晓东 <aa52105210@163.com>
2026-02-03 17:30:32 +0800

    初始化

--- huike-server/src/main/java/com/hk/boot/server/controller/DefaultController.java
+++ huike-server/src/main/java/com/hk/boot/server/controller/DefaultController.java
@@ -26,54 +26,10 @@ public class DefaultController {
                 "[工作流模块 huike-module-bpm - 已禁用][参考 https://doc.iocoder.cn/bpm/ 开启]");
     }
 
-    @RequestMapping("/admin-api/mp/**")
-    public CommonResult<Boolean> mp404() {
-        return CommonResult.error(NOT_IMPLEMENTED.getCode(),
-                "[微信公众号 huike-module-mp - 已禁用][参考 https://doc.iocoder.cn/mp/build/ 开启]");
-    }
-
-    @RequestMapping(value = { "/admin-api/product/**", // 商品中心
-            "/admin-api/trade/**", // 交易中心
-            "/admin-api/promotion/**" }) // 营销中心
-    public CommonResult<Boolean> mall404() {
-        return CommonResult.error(NOT_IMPLEMENTED.getCode(),
-                "[商城系统 huike-module-mall - 已禁用][参考 https://doc.iocoder.cn/mall/build/ 开启]");
-    }
-
-    @RequestMapping("/admin-api/erp/**")
-    public CommonResult<Boolean> erp404() {
-        return CommonResult.error(NOT_IMPLEMENTED.getCode(),
-                "[ERP 模块 huike-module-erp - 已禁用][参考 https://doc.iocoder.cn/erp/build/ 开启]");
-    }
-
     @RequestMapping("/admin-api/crm/**")
     public CommonResult<Boolean> crm404() {
         return CommonResult.error(NOT_IMPLEMENTED.getCode(),
                 "[CRM 模块 huike-module-crm - 已禁用][参考 https://doc.iocoder.cn/crm/build/ 开启]");
-    }
-
-    @RequestMapping(value = { "/admin-api/report/**"})
-    public CommonResult<Boolean> report404() {
-        return CommonResult.error(NOT_IMPLEMENTED.getCode(),
-                "[报表模块 huike-module-report - 已禁用][参考 https://doc.iocoder.cn/report/ 开启]");
-    }
-
-    @RequestMapping(value = { "/admin-api/pay/**"})
-    public CommonResult<Boolean> pay404() {
-        return CommonResult.error(NOT_IMPLEMENTED.getCode(),
-                "[支付模块 huike-module-pay - 已禁用][参考 https://doc.iocoder.cn/pay/build/ 开启]");
-    }
-
-    @RequestMapping(value = { "/admin-api/ai/**"})
-    public CommonResult<Boolean> ai404() {
-        return CommonResult.error(NOT_IMPLEMENTED.getCode(),
-                "[AI 大模型 huike-module-ai - 已禁用][参考 https://doc.iocoder.cn/ai/build/ 开启]");
-    }
-
-    @RequestMapping(value = { "/admin-api/iot/**"})
-    public CommonResult<Boolean> iot404() {
-        return CommonResult.error(NOT_IMPLEMENTED.getCode(),
-                "[IoT 物联网 huike-module-iot - 已禁用][参考 https://doc.iocoder.cn/iot/build/ 开启]");
     }
 
     /**
